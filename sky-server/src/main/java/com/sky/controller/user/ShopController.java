7a98a22e7664c504605337c0cc42532d089f1747
root <2571538285@qq.com>
2025-03-03 14:29:10 +0800

    设置商店关闭开启状态

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/admin/user")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     * 获取店铺状态
+     */
+    @ApiOperation("获取店铺状态")
+    @GetMapping("/status")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺状态:{}", status == 1 ? "开启" : "关闭");
+        return Result.success(status);
+    }
+
+}

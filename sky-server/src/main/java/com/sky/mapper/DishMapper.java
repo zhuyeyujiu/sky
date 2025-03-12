48e4c70b1e89469ba2c95551104decfffe5c0008
root <2571538285@qq.com>
2025-03-12 11:17:04 +0800

    控制台

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -81,4 +82,13 @@ public interface DishMapper {
      */
 
     List<Dish> list(Dish dish);
+
+
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
